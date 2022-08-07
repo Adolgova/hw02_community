3bbcd15a773fce84623997c0fdd1ff0624dd684d
Анастасия Долгова <a3894685@yandex.ru>
2022-08-07 23:07:00 +0300

    Отправка проекта

---
+++ yatube/posts/views.py
@@ -0,0 +1,27 @@
+
+# Create your views here.
+from cgitb import text
+from turtle import title
+from typing import Text
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    templates = 'posts/index.html'
+    context = {
+        'posts': posts,
+    }
+    return render(request, templates, context)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
