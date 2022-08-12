66088970e56b9b4687b84ed3c47a014e8bdde858
Анастасия Долгова <a3894685@yandex.ru>
2022-08-12 16:39:01 +0300

    Правки_2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,20 +6,18 @@ LIMIT_POST = 10
 
 
 def index(request):
-    template = 'posts/index.html'
     posts = Post.objects.order_by('-pub_date')[:LIMIT_POST]
     context = {
         'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:LIMIT_POST]
     context = {
         'group': group,
         'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
