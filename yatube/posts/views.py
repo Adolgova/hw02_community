c8c660301a9d2312d73deb1d097bff8a470fc8af
Анастасия Долгова <a3894685@yandex.ru>
2022-08-21 19:34:19 +0300

    когда все поломалось

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from posts.models import Post, Group
+from posts.models import Post, Group, User
 from django.core.paginator import Paginator
 
 
@@ -26,3 +26,27 @@ def group_posts(request, slug):
         'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    profile_list = Post.objects.filter(author=user)
+    count_posts = Post.objects.filter(author=user).count()
+    paginator = Paginator(profile_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {  
+        'user': user,
+        'profile_list': profile_list,
+        'count_posts': count_posts,
+        'page_obj': page_obj
+        }
+    return render (request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'post':post,
+    }
+    return render(request, 'posts/post_detail.html', context) 
